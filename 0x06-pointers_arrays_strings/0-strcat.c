6a6b727e1035e9daeb31eb46714332d662720297
Aymen <aymenabdu82@gmail.com>
2022-10-02 05:53:49 -0400

    more on pointer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,24 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * *_strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
- * Return: void
+* _strcat - concatenates two strings
+* @dest: pointer destination
+* @src: pointer source
+* Return: dest
 */
 
 char *_strcat(char *dest, char *src)
 {
-int a = -1, i;
-for (i = 0; dest[i] != '\0'; i++)
-;
+	int i, j = -1;
 
-do {
-	a++;
-	dest[i] = src[a];
-	i++;
-} while (src[a] != '\0');
+	for (i = 0; dest[i] != '\0'; i++)
+	;
 
-return (dest);
+	do {
+		j++;
+		dest[i] = src[j];
+		i++;
+	} while (src[j] != '\0');
+
+	return (dest);
 }
